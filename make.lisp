b3ee55c73975a1a55fb2c6a89ca369ee12f3056d
Shanrui <shanrui0321@163.com>
2015-07-09 16:54:32 +0800

    Update make.lisp
    windows下make遇到的小问题

--- make.lisp
+++ make.lisp
@@ -1,9 +1,9 @@
 ;(compile-file "init.lisp")
 (load "init.lisp")
 (let ((filename #+windows "linker.exe" 
-		(or #+darwin "linker"
-        #+unix "linker"
-		#+linux "linker"))
+		#+darwin "linker"
+		#+unix "linker"
+		#+linux "linker")
         (main #'my-game:linker))
        #+clisp (saveinitmem filename :init-function main :executable t :norc t)
             #+sbcl (save-lisp-and-die filename :toplevel main :executable t)
